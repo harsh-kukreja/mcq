3ca0a0cf50d0ddd67b1aa615a5c0e0d5a33be256
harsh-kukreja <harshkukreja99@gmail.com>
2019-07-27 16:56:30 +0530

    Batch Class Error Solved

--- models/Batch.class.php
+++ models/Batch.class.php
@@ -13,8 +13,6 @@ class Batch{
     public function __construct(){
         $this->pdo = $this->includes();
     }
-
-<<<<<<< HEAD
     /**
      * convertToString():
      * Method that converts a item variable into  a string
@@ -28,9 +26,6 @@ class Batch{
         return $string_item;
 
     }
-=======
->>>>>>> 0720768f8d52d4cf22f523b1c7de3471eba8a863
-
     /**
      * includes():
      * Method that includes helper class and crud class
@@ -41,10 +36,7 @@ class Batch{
     function includes(){
         include_once ("../helpers/helper.class.php");
         include "../includes/Crud.class.php";
-<<<<<<< HEAD
-=======
         include_once "../includes/functions.php";
->>>>>>> 0720768f8d52d4cf22f523b1c7de3471eba8a863
         $pdo = new Crud();
         return $pdo;
     }
@@ -62,11 +54,7 @@ class Batch{
 
     function createBatch($division_id,$batch_name,$created_by,$updated_by,$deleted=0,$additional_description="null"){
         $rows = array("division_id","batch_name","created_by","updated_by","deleted","additional_description");
-<<<<<<< HEAD
-        $values = array($this->convertToSTring($division_id),$this->convertToSTring($batch_name),$this->convertToSTring($created_by),$this->convertToSTring($updated_by),$deleted,$additional_description);
-=======
         $values = array(convertToSTring($division_id),convertToSTring($batch_name),convertToSTring($created_by),convertToSTring($updated_by),$deleted,$additional_description);
->>>>>>> 0720768f8d52d4cf22f523b1c7de3471eba8a863
 
         $this->pdo->insertDb("batch",$rows,$values);
 
@@ -83,11 +71,7 @@ class Batch{
      */
     function updateBatch($division_id,$batch_name,$created_by,$updated_by,$condition,$additional_description="null"){
 
-<<<<<<< HEAD
-        $field= array("division_id = ".$this->convertToString($division_id),"batch_name = ".$this->convertToString($batch_name),"created_by = ".$this->convertToSTring($created_by),"updated_by = ".$this->convertToSTring($updated_by),"additional_description = ".$additional_description);
-=======
         $field= array("division_id = ".convertToString($division_id),"batch_name = ".convertToString($batch_name),"created_by = ".convertToSTring($created_by),"updated_by = ".convertToSTring($updated_by),"additional_description = ".$additional_description);
->>>>>>> 0720768f8d52d4cf22f523b1c7de3471eba8a863
 
         $this->pdo->updateDb("batch",$field,$condition);
 
@@ -109,11 +93,7 @@ class Batch{
      * getDivision("21");
      */
     function getDivision($batch_id){
-<<<<<<< HEAD
-        $query = "Select division_id from batch where batch_id = $batch_id";
-=======
         $query = "SELECT division_id FROM batch WHERE batch_id = $batch_id";
->>>>>>> 0720768f8d52d4cf22f523b1c7de3471eba8a863
         $pdoObject = new PdoConnection();
         $connection = $pdoObject->connectPdo();
         $statement = $connection->prepare($query);
