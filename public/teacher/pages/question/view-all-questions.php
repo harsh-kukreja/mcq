b5e11e3d520288c73a0ec4d86fa95db5158c2b09
Dhiresh <dhireshk.hirani@gmail.com>
2019-07-28 15:09:04 +0530

    Teachers sidenav

---
+++ public/teacher/pages/question/view-all-questions.php
@@ -0,0 +1,35 @@
+<div class="row py-4">
+    <div class="col-md-12">
+        <a href="question.php?source=add_question" class="btn btn-primary">Add Question</a>
+    </div>
+</div>
+
+
+<?php
+
+	    
+?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Question</th>
+                    <th>Options</th>
+                    <th>Correct option</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
