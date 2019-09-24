6270aa9193167e0b5608768934ce1132fcd15a42
shahvrushali22 <shahvrushali22@gmail.com>
2019-09-24 23:38:09 +0530

    Removed backend code from user side

--- models/Test.class.php
+++ models/Test.class.php
@@ -63,6 +63,51 @@ class Test{
         $this->pdoObject->insertDb("test_questions",$rows,$values);
     }
 
+    /**
+     *
+     * @param $user_id :- Logged in User_id
+     * @return array :- Details of tests scheduled for the specific user_id
+     * Eg:- (Array ( [0] => Array ( [test_id] => 1 [test_name] => Java overview
+     * [teacher_id] => 1 [duration] => 00:15:00 [passing_marks] => 4
+     * [total_marks] => 15 [start_time] => 2019-09-24 23:40:00
+     * [end_time] => 2019-09-24 23:45:00 [type] => 0
+     * [negative_marks] => 0 [subject] => 20
+     * [created_by] => 2 [created_at] => 2019-04-21 15:07:49
+     *[updated_by] => 2 [updated_at] => 2019-04-21 15:07:49
+     * [deleted] => 0 [additional_description] => ) )
+     *
+     */
+    public function showUpcomingTest($user_id){
+        $query = "select * from test where (start_time >= SYSDATE() or end_time >= SYSDATE()) and test_id  in (select test_id from test_participants where student_id  = (select student_id from student where user_id  = ".$user_id. "))";
+
+        $pdoObject = new PdoConnection();
+        $connection = $pdoObject->connectPdo();
+        $statement = $connection->prepare($query);
+        $statement->execute();
+
+        $tests = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+        return $tests;
+    }
+
+    /**
+     * @param $teacher_id:- Id of specific teacher who has scheduled test for that particular user
+     * @return array:- Name of teachet
+     * Eg:- Array ( [0] => Array ( [first_name] => Kennedy [last_name] => Valencia ) )
+     *
+     */
+    public function getTeacherName($teacher_id){
+        $query = "select first_name,last_name from person where person_id  = (select person_id from user where user_id  = (select user_id from teacher where teacher_id = $teacher_id))";
+        $pdoObject = new PdoConnection();
+        $connection = $pdoObject->connectPdo();
+        $statement = $connection->prepare($query);
+        $statement->execute();
+        $teacher_name = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $teacher_name;
+    }
+
 }
 
 ?>
--- public/student/index.php
+++ public/student/index.php
@@ -6,7 +6,6 @@
  * Time: 16:27
  */
 
-
 ?>
 
 <!DOCTYPE html>
@@ -21,6 +20,7 @@ include_once($_SERVER["DOCUMENT_ROOT"]."/mcq/helpers/Helper.class.php");
 $helper = new Helper();
 //    include_once ("../../includes/header.php");
 include_once ($helper->getBasePath()."includes/header.php");
+include_once($_SERVER["DOCUMENT_ROOT"]."/mcq/models/Test.class.php");
 session_start();
 if(isset($_SESSION['role_id'])) {
     if ($_SESSION['role_id'] == 2) {
@@ -58,47 +58,48 @@ if(isset($_SESSION['role_id'])) {
                         </div>
 
                         <!-- Card stats -->
-    <?php
 
-        $query = "select * from test where (start_time >= SYSDATE() or end_time >= SYSDATE()) and test_id  =(select test_id from test_participants where student_id  = (select student_id from student where user_id  = ". $_SESSION['user_id'] . "))";
-        $pdoObject = new PdoConnection();
-        $connection = $pdoObject->connectPdo();
-        $statement = $connection->prepare($query);
-        $statement->execute();
-       while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-        $test_name = $row['test_name'];
-        $teacher_id  = $row['teacher_id'];
-        $duration = $row['duration'];
-        $total_marks = $row['total_marks'];
-        $subject_id = $row['subject'];
-        $start_time = $row['start_time'];
-        $end_time = $row['end_time'];
-        $query = "select first_name,last_name from person where person_id  = (select person_id from user where user_id  = (select user_id from teacher where teacher_id = $teacher_id))";
-        $pdoObject = new PdoConnection();
-        $connection = $pdoObject->connectPdo();
-        $statement = $connection->prepare($query);
-        $statement->execute();
-        $first_name ="";
-        $last_name  ="";
-        while($row  = $statement->fetch(PDO::FETCH_ASSOC)) {
-            $first_name = $row['first_name'];
-            $last_name = $row['last_name'];
-//            echo $row['last_name'];
-        }
-
-
-?>
                         <div class="row owl-carousel owl-dots owl-theme">
+                        <?php
+
+                            $test = new Test();
+                            $tests = $test->showUpcomingTest($_SESSION['user_id']);
+                            for($i = 0;$i< sizeof($tests); $i++){
+                                $test_name = $tests[$i]['test_name'];
+                                $teacher_id = $tests[$i]['teacher_id'];
+                                $duration = $tests[$i]['duration'];
+                                $total_marks = $tests[$i]['total_marks'];
+                                $subject_id = $tests[$i]['subject'];
+                                $start_time = $tests[$i]['start_time'];
+                                $end_time = $tests[$i]['end_time'];
+                                $teacher_name  = $test->getTeacherName($teacher_id);
+                                $first_name = "";
+                                $last_name = "";
+                                for($i = 0;$i<sizeof($teacher_name);$i++){
+                                    $first_name = $teacher_name[$i]['first_name'];
+                                    $last_name = $teacher_name[$i]['last_name'];
+
+                                }
+                                ?>
                             <div class="">
+
                                 <div class="card card-stats">
                                     <!-- Card body -->
+
                                     <div class="card-body">
+
                                         <div class="row">
+
+
                                             <div class="col">
 
-                                                <h5 class="card-title text-uppercase text-muted mb-0"><?php echo "By ".$first_name;echo " "; echo $last_name;?></h5>
-                                                <span class="h2 font-weight-bold mb-0"><?php echo $test_name?></span>
-                                                <div class="h5 font-weight-bold mb-0"><?php echo $total_marks?> MARKS</div>
+
+                                                <h5 class="card-title text-uppercase text-muted mb-0"><?php echo "By " . $first_name;
+                                                    echo " ";
+                                                    echo $last_name; ?></h5>
+                                                <span class="h2 font-weight-bold mb-0"><?php echo $test_name ?></span>
+                                                <div class="h5 font-weight-bold mb-0"><?php echo $total_marks ?>MARKS
+                                                </div>
                                             </div>
                                             <div class="col-auto">
                                                 <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
@@ -110,112 +111,47 @@ if(isset($_SESSION['role_id'])) {
                                             <?php date_default_timezone_set("Asia/Calcutta");
                                             $date = date('Y-m-d H:i:s:u', time());
 
-                                            if($start_time <= $date && $end_time >= $date){?>
+                                            if ($start_time <= $date && $end_time >= $date) {
+                                                ?>
                                                 <button type="button" class="btn btn-outline-primary pl-4 pr-4">Start
-                                            </button>
-                                          <?php  }else{
+                                                </button>
+                                            <?php } else {
                                                 $start = strtotime($start_time);
-                                                $curr_date =  date('Y-m-d',time());
-                                                $start_date = date('Y-m-d',$start);
-                                                if($curr_date == $start_date){
-                                                    echo "Starts at ".date('h:ia',$start);
-                                                }else{
-                                                    echo "Starts at ".date("d-M-y h:ia");
+                                                $curr_date = date('Y-m-d', time());
+                                                $start_date = date('Y-m-d', $start);
+                                                if ($curr_date == $start_date) {
+                                                    echo "Starts at " . date('h:ia', $start);
+                                                } else {
+                                                    echo "Starts at " . date("d-M-y h:ia");
                                                 }
-
-//
-
-
-
-
-//
-
 
 
                                             }
+
 
                                             ?>
 
 
                                         </p>
+
                                     </div>
+
                                 </div>
+
                             </div>
+
                             <?php }?>
-<!---->
-<!--                            <div class="">-->
-<!--                                <div class="card card-stats">-->
-<!--                                     -->
-<!--                                    <div class="card-body">-->
-<!--                                        <div class="row">-->
-<!--                                            <div class="col">-->
-<!--                                                <h5 class="card-title text-uppercase text-muted mb-0">Jessica snow</h5>-->
-<!--                                                <span class="h2 font-weight-bold mb-0">AOA</span>-->
-<!--                                                <div class="h5 font-weight-bold mb-0">20 MARKS</div>-->
-<!--                                            </div>-->
-<!--                                            <div class="col-auto">-->
-<!--                                                <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">-->
-<!--                                                    <i class="ni ni-active-40"></i>-->
-<!--                                                </div>-->
-<!--                                            </div>-->
-<!--                                        </div>-->
-<!--                                        <p class="mt-3 mb-0 text-sm text-center">-->
-<!--                                            <button class="btn btn-primary" type="button">View</button>-->
-<!--                                        </p>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                            </div>-->
 
-<!--                            <div class="">-->
-<!--                                <div class="card card-stats">-->
-<!--                                    -->
-<!--                                    <div class="card-body">-->
-<!--                                        <div class="row">-->
-<!--                                            <div class="col">-->
-<!--                                                <h5 class="card-title text-uppercase text-muted mb-0">Jessica snow</h5>-->
-<!--                                                <span class="h2 font-weight-bold mb-0">AOA</span>-->
-<!--                                                <div class="h5 font-weight-bold mb-0">20 MARKS</div>-->
-<!--                                            </div>-->
-<!--                                            <div class="col-auto">-->
-<!--                                                <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">-->
-<!--                                                    <i class="ni ni-active-40"></i>-->
-<!--                                                </div>-->
-<!--                                            </div>-->
-<!--                                        </div>-->
-<!--                                        <p class="mt-3 mb-0 text-sm text-center">-->
-<!--                                            <button class="btn btn-primary" type="button">View</button>-->
-<!--                                        </p>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                            </div>-->
+                  </div>
 
-                            <div class="">
-<!--                                <div class="card card-stats">-->
-<!--                                   -->
-<!--                                    <div class="card-body">-->
-<!--                                        <div class="row">-->
-<!--                                            <div class="col">-->
-<!--                                                <h5 class="card-title text-uppercase text-muted mb-0">Jessica snow</h5>-->
-<!--                                                <span class="h2 font-weight-bold mb-0">AOA</span>-->
-<!--                                                <div class="h5 font-weight-bold mb-0">20 MARKS</div>-->
-<!--                                            </div>-->
-<!--                                            <div class="col-auto">-->
-<!--                                                <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">-->
-<!--                                                    <i class="ni ni-active-40"></i>-->
-<!--                                                </div>-->
-<!--                                            </div>-->
-<!--                                        </div>-->
-<!--                                        <p class="mt-3 mb-0 text-sm text-center">-->
-<!--                                            <button class="btn btn-primary" type="button">View</button>-->
-<!--                                        </p>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-                            </div>
 
-                        </div>
                     </div>
+
                 </div>
+
             </div>
+
+
             <!-- Page content -->
             <div class="container-fluid mt--6">
                 <div class="row ">
